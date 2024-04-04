790313a919b63ab004743ac0f7182c515f045912
Jos Bogan <Josbogan@gmail.com>
2024-04-04 20:58:12 +0100

    initial commit

--- main.js
+++ main.js
@@ -8,12 +8,36 @@ const setup = () => {
         const emailInput = document.getElementById('email-input').value
         const phoneInput = document.getElementById('phone-input').value
         const messageBody = document.getElementById('message-input').value
-        // const submitInput = document.getElementById('submit-button')
 
-        console.log(nameInput, emailInput, phoneInput, messageBody)
         console.log('here')
-        window.open(`mailto:test@example.com?subject=subject&body=body`)
+        window.open(
+            `mailto:test@example.com?subject=New Enquiry: ${nameInput}&body=Name: ${nameInput}\n\nPhone:${
+                phoneInput || 'XXXX'
+            }\n\nEmail: ${emailInput || 'XXXX'}\n\n${messageBody}`
+        )
     })
+    addMap()
+}
+
+const addMap = () => {
+    const greenIcon = L.icon({
+        iconUrl: './assets/svgs/map-pin.svg',
+
+        iconSize: [40, 90], // size of the icon
+        iconAnchor: [20, 70], // point of the icon which will correspond to marker's location
+    })
+
+    const map = L.map('map').setView([51.617898, -0.185547], 13)
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map)
+
+    L.marker([51.617898, -0.185547], {
+        icon: greenIcon,
+    }).addTo(map)
 }
 
 setup()
+addMap()
