6f9a9b8c527e0a8d50ea31e432f61cb779242007
Jos Bogan <Josbogan@gmail.com>
2024-05-14 22:04:48 +0100

    added responsivness for mobile ad emailjs integration

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const setup = () => {
     const form = document.getElementById('form')
 
+    setupEmail()
+
     form.addEventListener('submit', (event) => {
         event.preventDefault()
 
@@ -9,20 +11,32 @@ const setup = () => {
         const phoneInput = document.getElementById('phone-input').value
         const messageBody = document.getElementById('message-input').value
 
-        console.log('here')
-        window.open(
-            `mailto:test@example.com?subject=New Enquiry: ${nameInput}&body=Name: ${nameInput}\n\nPhone:${
-                phoneInput || 'XXXX'
-            }\n\nEmail: ${emailInput || 'XXXX'}\n\n${messageBody}`
-        )
+        emailjs.send('XXXXX', 'XXXXX', {
+            enquiry_name: nameInput || 'XXXXX',
+            enquiry_phone: phoneInput || 'XXXXX',
+            enquiry_email: emailInput || 'XXXXX',
+            message: messageBody || 'XXXXX',
+        })
     })
     addMap()
+}
+
+const setupEmail = () => {
+    emailjs.init({
+        publicKey: 'XXXXX', // CHANGE ME
+        blockHeadless: true,
+        limitRate: {
+            // Set the limit rate for the application
+            id: 'app',
+            // Allow 1 request per 100s
+            throttle: 100000,
+        },
+    })
 }
 
 const addMap = () => {
     const greenIcon = L.icon({
         iconUrl: './assets/svgs/map-pin.svg',
-
         iconSize: [40, 90], // size of the icon
         iconAnchor: [20, 70], // point of the icon which will correspond to marker's location
     })
